ff8811d07fa50406b46da14d38c520a4cd4b3267
Raul Srdoc <raul.srdoc6@gmail.com>
2022-07-20 13:11:23 +0200

    Add project template

---
+++ src/main/java/com/agency04/devcademy/SpringBootProjectTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.agency04.devcademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProjectTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProjectTemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agency04/devcademy/SpringBootProjectTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agency04.devcademy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootProjectTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
