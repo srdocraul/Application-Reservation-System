167a2cc9c310f3fd3a7e0ec04c094eee66e441e2
Raul Srdoc <raul.srdoc6@gmail.com>
2022-07-24 15:34:18 +0200

    Add apartment controller

---
+++ src/main/java/com/agency04/devcademy/controllers/ApartmentController.java
@@ -0,0 +1,24 @@
+package com.agency04.devcademy.controllers;
+
+import com.agency04.devcademy.repositories.ApartmentRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ApartmentController {
+
+    private final ApartmentRepository apartmentRepository;
+
+    public ApartmentController(ApartmentRepository apartmentRepository) {
+        this.apartmentRepository = apartmentRepository;
+    }
+
+    @RequestMapping("/apartments")
+    public String getApartment(Model model ){
+
+        model.addAttribute("apartments", apartmentRepository.findAll());
+
+        return "apartments";
+    }
+}
