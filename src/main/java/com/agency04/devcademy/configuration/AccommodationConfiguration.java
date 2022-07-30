2eb5bdbc3e8d55c42cb358092c3adef2b39ae0bd
Raul Srdoc <raul.srdoc6@gmail.com>
2022-07-30 18:19:45 +0200

    Add primary annotation in configuration

--- src/main/java/com/agency04/devcademy/configuration/AccommodationConfiguration.java
+++ src/main/java/com/agency04/devcademy/configuration/AccommodationConfiguration.java
@@ -6,19 +6,17 @@ import com.agency04.devcademy.service.RoomAccommodationServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.Primary;
 
 @ImportResource("classpath:mobileHomeAccommodationService.xml")
 @Configuration
 public class AccommodationConfiguration {
-    MobileHomeAccommodationServiceImpl mobileHomeAccommodationService() {
-        return new MobileHomeAccommodationServiceImpl();
-    }
-    
     @Bean
     RoomAccommodationServiceImpl roomAccommodationService() {
         return new RoomAccommodationServiceImpl();
     }
 
+    @Primary
     @Bean
     AccommodationServiceImpl accommodationService() {
         return new AccommodationServiceImpl();
