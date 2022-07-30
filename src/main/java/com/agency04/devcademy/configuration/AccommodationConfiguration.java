3b3191d5f47692dba1ca7bec9c423861a3410899
Raul Srdoc <raul.srdoc6@gmail.com>
2022-07-30 14:27:31 +0200

    Add spring bean with java and xml

--- src/main/java/com/agency04/devcademy/configuration/AccommodationConfiguration.java
+++ src/main/java/com/agency04/devcademy/configuration/AccommodationConfiguration.java
@@ -1,14 +1,26 @@
 package com.agency04.devcademy.configuration;
 
 import com.agency04.devcademy.service.AccommodationServiceImpl;
+import com.agency04.devcademy.service.MobileHomeAccommodationServiceImpl;
+import com.agency04.devcademy.service.RoomAccommodationServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
 
+@ImportResource("classpath:mobileHomeAccommodationService.xml")
 @Configuration
 public class AccommodationConfiguration {
+    MobileHomeAccommodationServiceImpl mobileHomeAccommodationService() {
+        return new MobileHomeAccommodationServiceImpl();
+    }
+    
+    @Bean
+    RoomAccommodationServiceImpl roomAccommodationService() {
+        return new RoomAccommodationServiceImpl();
+    }
 
     @Bean
-    AccommodationServiceImpl accommodationService(){
+    AccommodationServiceImpl accommodationService() {
         return new AccommodationServiceImpl();
     }
 }
---
+++ src/main/java/com/agency04/devcademy/service/RoomAccommodationServiceImpl.java
@@ -0,0 +1,12 @@
+package com.agency04.devcademy.service;
+
+import com.agency04.devcademy.repository.AccommodationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class RoomAccommodationServiceImpl {
+
+    @Autowired
+    private AccommodationRepository accommodationRepository;
+
+
+}
